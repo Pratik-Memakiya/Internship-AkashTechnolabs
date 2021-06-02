dc24ee32ee2a470e1a73d7184c6bc59e0f165ba4
Pratik Memakiya <memakiyapratik@gmail.com>
2021-06-02 17:19:07 +0530

    task 5 and report added

---
+++ task/task5/t5.py
@@ -0,0 +1,25 @@
+'''
+Consider an employee class, which contains fields such as name and
+designation. And a subclass, which contains a field salary. Write a
+program for inheriting this relation.
+'''
+
+class Employee:
+    name ="ABCDEFG"
+    designation ="HR Manager"
+    def display(self):
+        print('name : ',self.name)
+        print('designation : ',self.designation)
+
+class Subclass(Employee):
+    salary=10000
+    def display1(self):
+       print('salary : ',self.salary)
+
+
+
+obj2 = Subclass()
+
+print("-------Subclass display()---------")
+obj2.display()
+obj2.display1()
